f9a46abb2c025ad9881226ae40778a9623818dfb
Raghu Betina <raghu@firstdraft.com>
2020-07-24 15:13:19 +0000

    Generate graphiti resources for posts

---
+++ app/resources/post_resource.rb
@@ -0,0 +1,11 @@
+class PostResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :body, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
